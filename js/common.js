bef3ddc45e94bb9601f8d5ee20a70c90229cd9db
mon551 <99079052+mon551@users.noreply.github.com>
2022-03-04 22:12:58 +0900

    Update common.js

--- js/common.js
+++ js/common.js
@@ -2,7 +2,7 @@
 
 //共通パーツ読み込み
 $(function() {
-    $("#header").load("/common/header.html");
+    $("#header").load("common/header.html");
     $("#updated-info").load("/common/updated-info.html");
     $("#footer").load("/common/footer.html");
 });
