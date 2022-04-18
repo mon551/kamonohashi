77bf1729bd5d2373d1fa775c2c14f2cc7560858c
mon kamonohashi <monsa1229@gmail.com>
2022-04-18 22:58:23 +0900

    途中

--- js/sample-7.js
+++ js/sample-7.js
@@ -4,34 +4,70 @@ let comHand = document.querySelectorAll(".com-hand");
 let playerHand = document.querySelectorAll(".player-hand");
 
 // トランプの配列を作る
-let cards = [];
-for(let i=1; i<=4; i++) {
-    for(let j=1; j<=13; j++) {
-        cards[cards.length] = [i, j, false];
+let cards = Array(5);
+for(let i=0; i<=4; i++) {
+    cards[i] = Array(13);
+
+    // カードの初期値を代入
+    for(let j=0; j<13; j++) {
+        // iが0 && jが2以上だったらtrue
+        if(i!==0 || (i===0 && j<2) ){
+            cards[i][j] = false;
+        } else {
+            cards[i][j] = true;
+        }
     }
 }
 
-const newImg = getImgSrc();
+// 画面が読み込まれたら
+window.onload = () => {
+    // comの手札を生成
+    comHand.forEach( el => {
+        //とりあえず裏
+        el.innerHTML = "<img src=\"../img/cards/card-back.png\">";
+    });
 
+    // プレイヤーの手札を生成
+    playerHand.forEach(el => {
+        const img = getImgSrc();
 
-// comHandはNodelistだけどforEachは使える
-comHand.forEach( el => {
-    el.textContent("あ");
-    console.log(el);
-});
+        el.innerHTML = `${img}`;
+        el.addEventListener('click', function(){playCard(img, el);}, false);
+    });
+}
+
+// カードを出す
+function playCard(img, el) {
+    const comArea = document.querySelector(".play-com-card");
+    const playerArea = document.querySelector(".play-player-card");
+
+    playerArea.innerHTML = `${img}`;
+
+    const newImg = getImgSrc();
+
+    el.innerHTML = newImg;
+}
+
 
 
 // 画像を取得
 function getImgSrc() {
-    // imgタグを新しく作る
-    const newImg = document.createElement("img");
+    let suit, num;
 
-    // とりあえず裏面
-    newImg.src="../img/cards/card-back.png";
+    do{
+        const flg = cards.every(function(val){
+            return true;
+        });
 
-    // 画像サイズを整える(px)
-    newImg.width = 50;
-    newImg.height = 50;
+        // ランダム作成(記号)
+        suit = Math.floor(Math.random() * 4 + 1);
+        // ランダム作成(数字)
+        num = Math.floor(Math.random() * 12 + 1);
 
-    return newImg;
+    } while(cards[suit][num] == true)
+
+    // 使用済はtureにする
+    cards[suit][num] = true;
+
+    return `<img src="../img/cards/${suit}-${num}.png">`;
 }
